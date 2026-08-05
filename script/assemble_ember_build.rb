09cd0c0664fb51d73a059cd973561442947f62e9
larisps <automacaoprovip@gmail.com>
2026-08-04 23:05:31 -0300

    ci: build docker image via GitHub Actions (Railway build container too small for JS bundling)

--- script/assemble_ember_build.rb
+++ script/assemble_ember_build.rb
@@ -145,8 +145,8 @@ if Etc.nprocessors > 2
 end
 
 if low_memory_environment?
-  log "Node.js heap_size_limit is less than 2048MB. Setting --max-old-space-size=600 and CHEAP_SOURCE_MAPS=1"
-  build_env["NODE_OPTIONS"] = "--max_old_space_size=600"
+  log "Node.js heap_size_limit is less than 2048MB. Setting --max-old-space-size=2048 and CHEAP_SOURCE_MAPS=1"
+  build_env["NODE_OPTIONS"] = "--max_old_space_size=2048"
   build_env["CHEAP_SOURCE_MAPS"] = "1"
   build_env["JOBS"] = "1"
 end
